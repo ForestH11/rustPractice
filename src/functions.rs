1a7377c1ecb549e2efb96ffe5042190068cef9b8
Benjamin Jenson <benjamin.jenson11@yahoo.com>
2022-05-24 22:28:13 -0600

    added README

--- src/functions.rs
+++ src/functions.rs
@@ -1,9 +1,23 @@
 fn main() {
     println!("Hello, World!");
-
+    let s = String::from("Hello");
+    let x = 5;
     another_function();
+    takeOwnership(s);
+    takeOwnership(x);
+    // The following line of code does not work
+    //because s is no longer in scope. The function
+    //takeOwnership has s now, and since it is a
+    //String type, there is no copy made.
+    // println!("{}",s);
+    println!("{}",x);
 }
 
 fn another_function() {
     println!("Another Function")
+}
+
+fn takeOwnership(s: String) {
+    println!("{}",s);
+    s;
 }
